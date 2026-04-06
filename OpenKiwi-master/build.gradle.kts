dd36d9ce8f40e7da48895e63b9fee459f3bf3203
happy-everyday-everyweek <18804145766@139.com>
2026-04-06 23:01:45 +0800

    Delete OpenKiwi-master directory

--- OpenKiwi-master/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.ksp) apply false
-    id("com.chaquo.python") version "17.0.0" apply false
-}
